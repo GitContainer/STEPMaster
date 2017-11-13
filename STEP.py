083716a0858b08d4b3e78ddeb09f0bcdafe2158e
Armia Faizal Sababa <armyids@gmail.com>
2017-11-13 14:21:35 +0700

    Version 3.0.0
    Update command to scrape main file.

--- STEP.py
+++ STEP.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 os.chdir("main/")
+subprocess.call("python flush.py", shell=True)
 subprocess.call("python check.py", shell=True)
 subprocess.call("python splasherosion.py", shell=True)
 subprocess.call("python sheeterosion.py", shell=True)
